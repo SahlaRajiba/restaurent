676b0acccbeb073f8cb853c61a96528787568752
Sahla Rajiba <sahlarajiba@gmail.com>
2023-07-07 18:51:14 +0530

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import "./Home.css";
+
+export default function App() {
+  // c
+
+  return (
+    <div>
+      <div className="img-div">
+        <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F262978%2Fpexels-photo-262978.jpeg%3Fcs%3Dsrgb%26dl%3Dpexels-pixabay-262978.jpg%26fm%3Djpg&tbnid=BJcE9wzxzqXDNM&vet=12ahUKEwiFlpaVyfz_AhUY7DgGHTBNBEIQMygCegUIARDNAQ..i&imgrefurl=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Frestaurant%2520food%2F&docid=a9b11MI_V60KkM&w=5616&h=3744&q=restaurant%20images%20free%20download&ved=2ahUKEwiFlpaVyfz_AhUY7DgGHTBNBEIQMygCegUIARDNAQ" alt="" />
+      </div>
+      <Container>
+      <h1 className="title">TASTYBITES</h1>
+        {/* {*/}
+      </Container>
+    </div>
+  );
+}
---
+++ src/Login.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+export default function Login() {
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+  
+    async function submit(e) {
+      e.preventDefault();
+  
+      try {
+        const response = await axios.post("http://localhost:3001/", {
+          email,
+          password
+        });
+  
+        if (email === "admin" && password === "admin") {
+          navigate("/HomeAdmin");
+        } else if (response.data === "exist") {
+          navigate("/Home", { state: { id: email } });
+        } else if (response.data === "notexist") {
+          alert("User has not signed up");
+        }
+      } catch (error) {
+        alert("Wrong details");
+        console.log(error);
+      }
+    }
+  
+    return (
+      <div>
+        <div style={{ backgroundImage: "url(/Images/bg)" }}>
+          <div style={{ fontWeight: '700', fontSize: '40px', textAlign: 'center', marginTop: '60px' }}>
+            “Food is not just eating <br /> It's an experience”
+          </div>
+          <div style={{ backgroundColor: 'black', width: '470px', height: '320px', margin: 'auto', borderRadius: '30px', marginTop: '30px' }}>
+            <img
+            //   src=
+              alt=""
+              style={{ marginLeft: '42%', marginTop: '15px', height: '90px' }}
+            />
+            <input
+              type='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder='Email'
+              style={{ backgroundColor: 'white', width: '425px', height: '60px', borderRadius: '20px', marginTop: '15px', marginBottom: '30px', marginLeft: '22px', paddingLeft: '50px', color: '#D4D0D0' }}
+            />
+            <input
+              type='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder='Password'
+              style={{ backgroundColor: 'white', width: '425px', height: '60px', borderRadius: '20px', marginTop: '4px', marginLeft: '22px', paddingLeft: '50px', color: '#D4D0D0' }}
+            />
+          </div>
+          <button type='submit' onClick={submit} style={{ backgroundColor: 'black', marginLeft: '46.8%', marginTop: '50px', borderRadius: '15px', fontWeight: '500', padding: '5px 15px 5px 15px', fontSize: '20px', color: 'white' }}>Login</button>
+          <p style={{ textAlign: 'center', marginTop: '10px' }}>
+            Don't have an account? <a href="/Signup">Sign up</a>
+          </p>
+        </div>
+      </div>
+    );
+  }
+  
